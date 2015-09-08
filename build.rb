710520db7e210363500993c2c442b58800fd2b9e
Michael Berkovich <michael@translationexchange.com>
2015-09-08 13:02:48 -0700

    fixed tagging

--- build.rb
+++ build.rb
@@ -6,12 +6,18 @@ def run(cmd)
   system(cmd)
 end
 
+run('git checkout master')
+run('git merge develop')
+run('git push')
+
 run('bundle exec rspec')
 run('gem build tml.gemspec')
 run("gem install tml-#{Tml::VERSION}.gem --no-ri --no-rdoc")
 
 if ARGV.include?('release')
-  run('git tag #{TmlRails::VERSION}')
+  run("git tag #{TmlRails::VERSION}")
   run('git push')
   run("gem push tml-#{Tml::VERSION}.gem")
 end
+
+run('git checkout develop')
