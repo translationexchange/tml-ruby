c27a0d77ff50792700674409e259131589c9d174
Michael Berkovich <michael@translationexchange.com>
2015-10-05 16:42:08 -0700

    updated version

--- build.rb
+++ build.rb
@@ -15,7 +15,7 @@ run('gem build tml.gemspec')
 run("gem install tml-#{Tml::VERSION}.gem --no-ri --no-rdoc")
 
 if ARGV.include?('release')
-  run("git tag #{TmlRails::VERSION}")
+  run("git tag #{Tml::VERSION}")
   run('git push')
   run("gem push tml-#{Tml::VERSION}.gem")
 end
