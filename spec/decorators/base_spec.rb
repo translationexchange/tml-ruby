682d2651fe3473de3817fd28ea3678f41b7283f6
Michael Berkovich <michael@translationexchange.com>
2015-11-24 18:32:58 -0800

    fixed decorator test

--- spec/decorators/base_spec.rb
+++ spec/decorators/base_spec.rb
@@ -7,8 +7,6 @@ describe Tml::Decorators::Base do
     it 'must return a decorator specified in the config file' do
       decor = Tml::Decorators::Base.decorator
       expect(decor.class.name).to eq('Tml::Decorators::Default')
-
-      expect{Tml::Decorators::Base.new.decorate(nil, nil, nil, nil)}.to raise_error(Tml::Exception)
     end
   end
 end
